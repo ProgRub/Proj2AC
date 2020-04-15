6e01eea28ca018bfa418e74d879736d9e47d9de6
ProgRub <ruben2000jgr@gmail.com>
2020-04-15 15:25:48 +0100

    Ligacao das partes

--- Projeto.asm
+++ Projeto.asm
@@ -28,7 +28,7 @@ BateriaCarro EQU 06H ;aumento relativo ao inicio dos dados do aluno para ler qua
 Proximo EQU 08H ;salto a executar para ler os dados do próximo aluno
 Tamanho EQU 1 ;número de alunos na base de dados
 
-StackPointer EQU 6600H ;endereço da pilha
+StackPointer EQU 8000H ;endereço da pilha
 
 
     ;CALL Display_EstadoServico
@@ -37,80 +37,112 @@ StackPointer EQU 6600H ;endereço da pilha
 
 Main:
     MOV SP, StackPointer
-    CALL VerificaOK
+    MOV R3, EnderecoBateriaNormal
+    MOV R4, EnderecoBateriaSemiRapido
+    MOV R5, EnderecoBateriaRapido
+    MOV R0,[R3]
+    MOV R1, [R4]
+    MOV R2,[R5]
+    MOV R3,0
+    MOV R4,0
+    MOV R5,0
+    CALL Programa
     JMP Fim
 
+Programa:
+    CALL InsereEnergia
+    CALL NiveisDeEnergia
+    CALL Verificacao_Aluno
+    CALL EscolhaCarregamento
+    RET
 InsereEnergia:
-    MOV R0, InputIncrementoBateria ;R0 contém o endereço de onde se lê o input de quanto carregar a bateria
-    MOV R1, InputTipoCarregamento ;R1 contém o endereço de onde se lê o input de qual bateria carregar
+    PUSH R3
+    PUSH R4
+    PUSH R5
+    PUSH R6
+    MOV R5, InputIncrementoBateria ;R0 contém o endereço de onde se lê o input de quanto carregar a bateria
+    MOV R6, InputTipoCarregamento ;R1 contém o endereço de onde se lê o input de qual bateria carregar
     CALL VerificaOK
     MOV R4,0 ;reset do valor lido do endereço de OK
-    MOV R2, [R1] ;R2 contém a seleção de qual bateria carregar, por parte do utilizador
-    MOV R3, [R0] ;R3 contém o valor a adicionar à bateria selecionada, se possivel
+    MOV R3, [R5] ;R2 contém a seleção de qual bateria carregar, por parte do utilizador
+    MOV R4, [R6] ;R3 contém o valor a adicionar à bateria selecionada, se possivel
     MOV R5,CustoNormal
-    CMP R2, R5
+    CMP R3, R5
     JNE IncrementaSemiRapido ;se verificar-se que a bateria escolhida não é a normal, procede-se para a verificação das outras baterias
-    MOV R4, EnderecoBateriaNormal ;R4 contém o endereço onde se guarda o valor armazenado na bateria normal
-    MOV R5, [R4] ;R5 contém o valor de bateria armazenada
-    ADD R5, R3 ;adicionamos a R5 (bateria) o valor que o utilizador inseriu
+    ADD R0, R4 ;adicionamos a R5 (bateria) o valor que o utilizador inseriu
     JV OverflowBateria ;se ocorrer overflow informar
-    MOV [R4],R5 ;se não ocorrer overflow, atualizar a variável na memória que guarda o valor da bateria
-    JMP NiveisDeEnergia ;se não ocorrer overflow, avançar para o display dos niveis de energia
+    JMP FimFunc1 ;se não ocorrer overflow, avançar para o display dos niveis de energia
 IncrementaSemiRapido:
     MOV R5,CustoSemiRapido
-    CMP R2,R5
+    CMP R3,R5
     JNE IncrementaRapido ;verificar se escolheu carregar a bateria semirapida, se não avançar
-    MOV R4, EnderecoBateriaSemiRapido ;R4 contém o endereço onde se guarda o valor da bateria semirapida
-    MOV R5,[R4] ;R5 contém o valor da bateria armazenada
-    ADD R5,R3 ;adicionamos a R5 (bateria) o valor que o utilizador inseriu
+    ADD R1,R4 ;adicionamos a R5 (bateria) o valor que o utilizador inseriu
     JV OverflowBateria; se ocorrer overflow informar
-    MOV [R4],R5 ;se não ocorrer overflow, atualizar a variável na memória que guarda o valor da bateria
-    JMP NiveisDeEnergia ;avançar para o display dos niveis de energia
+    JMP FimFunc1 ;avançar para o display dos niveis de energia
 IncrementaRapido:
     MOV R5, CustoRapido
-    CMP R2, R5
-    JNE NiveisDeEnergia ;verificar se escolheu carregar a bateria rapida, se não avançar
-    MOV R4, EnderecoBateriaRapido ;R4 contém o endereço onde se guarda o valor da bateria rapida
-    MOV R5,[R4] ;R5 contém o valor da bateria armazenada
-    ADD R5,R3 ;adicionamos a R5 (bateria) o valor que o utilizador inseriu
+    CMP R3, R5
+    JNE FimFunc1 ;verificar se escolheu carregar a bateria rapida, se não avançar
+    ADD R2,R4 ;adicionamos a R5 (bateria) o valor que o utilizador inseriu
     JV OverflowBateria ;se ocorrer overflow informar
-    MOV [R4],R5 ;se não ocorrer overflow, atualizar a variável na memória que guarda o valor da bateria
-    JMP NiveisDeEnergia ;avançar para o display dos niveis de energia
+    JMP FimFunc1 ;avançar para o display dos niveis de energia
 OverflowBateria:
-    MOV R9,0
-    MOV [R4],R9
-    JMP Fim
+    MOV R6,0
+    MOV [R4],R6
+FimFunc1:
+    POP R6
+    POP R5
+    POP R4
+    POP R3
+    RET
     ; PLACE 0900H
     ; String "OVERFLOW"
     ; PLACE 0056H
 
 
 NiveisDeEnergia:
-    MOV R0,EnderecoBateriaNormal ;R0 guarda o endereço onde está o valor da bateria normal
-    MOV R1,EnderecoBateriaSemiRapido ;R1 guarda o endereço onde está o valor da bateria semirápida
-    MOV R2,EnderecoBateriaRapido ;R2 guarda o endereço onde está o valor da bateria rápida
-	MOV R3,[R0] ;R3 tem o valor da bateria do carregamento normal
-    MOV R4,[R1] ;R4 tem o valor da bateria do carregamento semirapido
-    MOV R5,[R2] ;R5 tem o valor da bateria do carregamento rápido 
-    MOV R6,0 ;R6 contará quantas bateria estiverem abaixo do nivel minimo
-    MOV R7,Normal ;R7 contém o nivel minimo que a bateria normal deve ter para ser considerada funcional
-    MOV R8,Semirapido ;R8 contém o nivel minimo que a bateria semirapida deve ter para ser considerada funcional
-    MOV R9,Rapido ;R9 contém o nivel minimo que a bateria rapida deve ter para ser considerada funcional
-    CMP R3,R7
+    PUSH R3
+    PUSH R4
+    PUSH R5
+    PUSH R6
+    PUSH R7    
+    MOV R3,0 ;R6 contará quantas bateria estiverem abaixo do nivel minimo
+    MOV R4,Normal ;R7 contém o nivel minimo que a bateria normal deve ter para ser considerada funcional
+    MOV R5,Semirapido ;R8 contém o nivel minimo que a bateria semirapida deve ter para ser considerada funcional
+    MOV R6,Rapido ;R9 contém o nivel minimo que a bateria rapida deve ter para ser considerada funcional
+    CMP R0,R4
     JGE VerificaSemiRapido ;se verificarmos que a bateria normal tem o nivel minimo, verificamos as restantes
-    ADD R6,1 ;caso contrario, adicionamos 1 ao contador
+    ADD R3,1 ;caso contrario, adicionamos 1 ao contador
 VerificaSemiRapido:
-    CMP R4,R8
+    CMP R1,R5
     JGE VerificaRapido ;se verificarmos que a bateria semirapida tem o nivel minimo, verificamos a bateria rapida
-    ADD R6,1 ;caso contrario, adicionamos 1 ao contador
+    ADD R3,1 ;caso contrario, adicionamos 1 ao contador
 VerificaRapido:
-    CMP R5, R9
-    JGT Verificacao_Aluno ;se verificarmos que a bateria rapida tem o nivel minimo, o posto esta operacional
-    ADD R6,1 ;caso contrario, adicionamos 1 ao contador
-    MOV R10,3 ;R10 guarda o valor 3 para comparar ao contador
-    CMP R6,R10
-    JEQ Fim ;se o contador for igual a 3, o posto esta inoperacional
+    CMP R2, R6
+    JGT FimFunc ;se verificarmos que a bateria rapida tem o nivel minimo, o posto esta operacional
+    ADD R3,1 ;caso contrario, adicionamos 1 ao contador
+    MOV R7,3 ;R10 guarda o valor 3 para comparar ao contador
+    CMP R3,R7
+    JNE FimFunc
+    CALL InsereEnergia
+FimFunc:
+    POP R7
+    POP R6
+    POP R5
+    POP R4
+    POP R3
+    RET
+
 Verificacao_Aluno:
+    PUSH R0
+    PUSH R1
+    PUSH R2
+    PUSH R3
+    PUSH R4
+    PUSH R5
+    PUSH R6
+    PUSH R7
+    PUSH R8
     MOV R0, Base_Tabela_Dados ;mover para R0 a base da tabela de dados, será a base dos dados do aluno que estamos a verifica e contém o ID deste
     MOV R1, 0 ;R1 será o índice
     MOV R2, CodSeguranca ;R2 será a posição na tabela onde está o código de segurança do aluno
@@ -126,19 +158,30 @@ Ciclo_Verify_Aluno:
     JNE VerificacaoFalhada ;se R7(ID do utilizador) for diferente de R3(ID a ser verificado na tabela), avançar para o próximo, se possivel
     CMP R6,R4
     JNE VerificacaoFalhada ;se R8(Código de segurança do utilizador) for diferente de R4(código de segurança a ser verificado na tabela), avançar para o próximo, se possivel
-    MOV R9,1; R9 indica que a verificação foi bem sucedida (1-true)
     MOV R10,R1 ;mete no registo 10 o indice do aluno verificado com sucesso, para ser utilizado posteriormente para verificar o seu saldo
-    JMP EscolhaCarregamento ;se não saltou anteriormente, então o utilizador foi verificado com sucesso e pode carregar o seu veículo
+    JMP FimFunc2 ;se não saltou anteriormente, então o utilizador foi verificado com sucesso e pode carregar o seu veículo
 VerificacaoFalhada:
+    MOV R10,-1
     ADD R1,2
     MOV R7,R1 ;R7 serve para verificar se o indice é igual ao tamanho
     MOV R8,Tamanho ;R8 é o número de alunos na base de dados
     SHR R7,1 ;dividir por 2, para a verificação do índice com o tamanho
     CMP R7,R8 
-    JEQ Fim ;chegou ao fim da base
+    JEQ FimFunc2 ;chegou ao fim da base
     MOV R3,Proximo
     ADD R0,R3 ;avanca a base para o proximo aluno a verificar
     JMP Ciclo_Verify_Aluno
+FimFunc2:
+    POP R8
+    POP R7
+    POP R6
+    POP R5
+    POP R4
+    POP R3
+    POP R2
+    POP R1
+    POP R0
+    RET
 	
 Fim:
     JMP Fim
@@ -154,7 +197,11 @@ CicloVerOK:
     POP R1
     POP R0
     RET
+
 EscolhaCarregamento: 
+    PUSH R0
+    PUSH R1
+    PUSH R2
 	MOV R0, CustoNormal								;coloca no registo 0 o valor do custo do carregamento do tipo normal
 	MOV R1, CustoSemiRapido							;coloca no registo 1 o valor do custo do carregamento do tipo semi-rápido
 	MOV R2, CustoRapido								;coloca no registo 2 o valor do custo do carregamento do tipo rápido
@@ -162,11 +209,11 @@ EscolhaCarregamento:
     MOV R5, InputTipoCarregamento ;coloca no registo 5 o endereço de onde ler o tipo de carregamento
 	MOV R3, [R5]					                ;coloca no registo 3 o tipo de carregamento escolhido pelo utilizador
 	CMP R3,R0										;compara o registo 3 com o registo 0
-	JEQ	EscolhaTempo								;verifica se a comparação anterior é verdadeira
+	CALL EscolhaTempo								;verifica se a comparação anterior é verdadeira
 	CMP R3, R1										;compara o registo 3 com o registo 1
-	JEQ	EscolhaTempo								;verifica se a comparação anterior é verdadeira
+	CALL EscolhaTempo								;verifica se a comparação anterior é verdadeira
 	CMP R3, R2										;compara o registo 3 com o registo 2
-	JEQ EscolhaTempo								;verifica se a comparação anterior é verdadeira
+	CALL EscolhaTempo								;verifica se a comparação anterior é verdadeira
 	JMP EscolhaCarregamento							;volta para o menu inicial
 	
 CarregamentoRapidoValido:
@@ -181,8 +228,9 @@ EscolhaTempo:
     MOV R6, InputTempo      ;coloca no registo 6 o endereço de onde ler quanto tempo carregar
 	MOV R4, [R6]							;coloca no registo 0 o tempo escolhido pelo utilizador
 	CMP R4, 0										;se o valor do registo 0 for superior a 0, verifica o saldo do utilizador 
-	JGT VerificaSaldo
-	JMP EscolhaTempo_VerificaOK								;volta para o menu inicial
+	JLE EscolhaTempo
+	CALL VerificaSaldo
+    RET
 	
 VerificaSaldo:									;coloca no registo 5 o custo do tipo de carregamento
 	MUL R3, R4										;multiplica o registo 5 com o tempo escolhido pelo utilizador
@@ -226,34 +274,23 @@ AtualizaValoresEnergia:
 AtualizaPostoNormal:
 	MOV R2, Normal ;coloca no registo 2 a energia do carregamento normal por hora
 	MUL R6, R2 ;coloca no registo 1 a energia do carregamento total
-	MOV R3, EnderecoBateriaNormal ;coloca no registo 3 o endereço de onde ler o valor da bateria normal
-	MOV R4, [R3] ;coloca no registo 4 o valor da bateria normal
-	SUB R4, R6 ;subtrai o valor da energia do carregamento total à bateria do posto normal
-	MOV [R3], R4 ;atualiza o valor de energia da bateria do posto normal
+	SUB R0, R6 ;subtrai o valor da energia do carregamento total à bateria do posto normal
 	JMP NiveisDeEnergia ;salta para o "tag" NiveisDeEnergia
 	
 AtualizaPostoSemiRapido:
 	MOV R2, Semirapido ;coloca no registo 2 a energia do carregamento semi-rapido por hora
 	MUL R6, R2 ;coloca no registo 1 a energia do carregamento total
-	MOV R3, EnderecoBateriaSemiRapido ;coloca no registo 3 o endereço de onde ler o valor da bateria semi-rapida
-	MOV R4, [R3] ;coloca no registo 4 o valor da bateria semi-rapida
-	SUB R4, R6  ;subtrai o valor da energia do carregamento total à bateria do posto semi-rapido
-	MOV [R3], R4 ;atualiza o valor de energia da bateria do posto semi-rapido
+	SUB R1, R6  ;subtrai o valor da energia do carregamento total à bateria do posto semi-rapido
 	JMP NiveisDeEnergia ;salta para o "tag" NiveisDeEnergia
 
 AtualizaPostoRapido:
 	MOV R2, Rapido ;coloca no registo 2 a energia do carregamento rapido por hora
 	MUL R6, R2 ;coloca no registo 1 a energia do carregamento total
 	MOV R3, EnderecoBateriaRapido ;coloca no registo 3 o endereço de onde ler o valor da bateria rapida
-	MOV R4, [R3] ;coloca no registo 4 o valor da bateria rapida
-	SUB R4, R6  ;subtrai o valor da energia do carregamento total à bateria do posto rapido
-	MOV [R3], R4 ;atualiza o valor de energia da bateria do posto rapido
+	SUB R2, R6  ;subtrai o valor da energia do carregamento total à bateria do posto rapido
 	JMP NiveisDeEnergia ;salta para o "tag" NiveisDeEnergia
 	
 	
-	
-; Fim:
-;     JMP Fim
 ; Inicio:
 ;     ;CALL Display_EstadoServico
 ;     CALL LimpaDisplay
